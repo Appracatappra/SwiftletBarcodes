5aa5997f49fc365cd469d4c8239425e82d279dc2
Kevin Mullins <kevin_mullins@appracatappra.com>
2021-06-17 12:21:03 -0500

    Expose SwiftletBarcodeView
    I forgot to properly expose the `SwiftletBarcodeView` by making it public.

--- Sources/SwiftletBarcodes/SwiftletBarcodeView.swift
+++ Sources/SwiftletBarcodes/SwiftletBarcodeView.swift
@@ -18,45 +18,45 @@ let card = store.loyaltyCard
 SwiftletBarcodeView(showTitle: true, title:card.name, showData: true, data: card.data, format: card.format, hasDivider: true, width: card.width, height: card.height)
 ```
  */
-struct SwiftletBarcodeView: View {
+public struct SwiftletBarcodeView: View {
     // MARK: - Properties
     /// If `true`, show the large title over the barcode.
-    var showTitle:Bool = true
+    public var showTitle:Bool = true
     
     /// The text for the large title.
-    var title = "Loyalty Card"
+    public var title = "Loyalty Card"
     
     /// The color to display the large title in.
-    var titleColor:Color = .black
+    public var titleColor:Color = .black
     
     /// If `true`, display the data represented by the barcode underneath it.
-    var showData:Bool = true
+    public var showData:Bool = true
     
     /// The data that the barcode will represent.
-    var data = "1 12208 81912 0"
+    public var data = "1 12208 81912 0"
     
     /// The color to show the represented data in.
-    var dataColor:Color = .black
+    public var dataColor:Color = .black
     
     /// The format of the barcode to generate.
-    var format:SwiftletBarcodes.BarcodeFormat = .code128
+    public var format:SwiftletBarcodes.BarcodeFormat = .code128
     
     /// If `true`, display a `Divider()` before and after the barcode.
-    var hasDivider:Bool = true
+    public var hasDivider:Bool = true
     
     /// The width of the barcode to generate.
-    var width:Double = 380
+    public var width:Double = 380
     
     /// The hieght of the barcode to generate.
-    var height:Double = 150
+    public var height:Double = 150
     
     /// Returns the image of the generated barcode.
-    var barcode:Image? {
+    public var barcode:Image? {
         return SwiftletBarcodes.generate(from: data, format: format, width: width, height:height)
     }
     
     /// Returns the body of the generated barcode.
-    var body: some View {
+    public var body: some View {
         VStack {
             if let barcode = barcode {
                 if hasDivider {
@@ -88,9 +88,9 @@ struct SwiftletBarcodeView: View {
 
 
 /// Defines a sample version of the `SwiftletBarcodeView` used during design.
-struct SwiftletBarcodeView_Previews: PreviewProvider {
+public struct SwiftletBarcodeView_Previews: PreviewProvider {
     /// Returns the sample previews for design.
-    static var previews: some View {
+    public static var previews: some View {
         SwiftletBarcodeView()
     }
 }
